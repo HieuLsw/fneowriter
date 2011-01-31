092135dabf4ab2d5272aa2b96a3014f07c9b6ec5
scr4free@gmail.com <scr4free@gmail.com@e5fee052-d77b-027c-6a6b-2018b007d91a>
2011-01-31 06:34:08 +0000

    rummikub project import

---
+++ trunk/rummikub_timer/Classes/HelloWorldScene.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  rummikub_timer
+//
+//  Created by Bright Choe on 11. 1. 31..
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorld Layer
+@interface HelloWorld : CCLayer
+{
+}
+
+// returns a Scene that contains the HelloWorld as the only child
++(id) scene;
+
+@end
---
+++ trunk/rummikub_timer/Classes/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  rummikub_timer
+//
+//  Created by Bright Choe on 11. 1. 31..
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
